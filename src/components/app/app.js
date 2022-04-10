dec7af2907dec6b57fc1cce38140f08f4e276265
zahar varfolomeev <83136866+Lucifer000014@users.noreply.github.com>
2022-04-10 21:35:20 +0300

    commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,30 +1,67 @@
-import './app.css'
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
-import SearchPanel from "../search-panel/search-panel";
-import AppFilter from "../app-filter/app-filter";
-import EmployeesList from "../employees-list/employees-list";
-import EmployeesAddForm from "../employees-add-form/employees-add-form";
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
-function App() {
+import './app.css';
 
-    const data = [
-        {name: "Alex T.", salary: 2100, increase: false},
-        {name: "Bob C.", salary: 4000, increase: false},
-        {name: "John R.", salary: 900, increase: true},
-    ]
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'John C.', salary: 800, increase: false, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+            ]
+        }
+        this.maxId = 4;
+    }
 
-    return (
-        <div className="app">
-            <AppInfo/>
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    // Да, пока могут добавляться пустые пользователи. Мы это еще исправим
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppInfo />
+
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+
+                <EmployeesList
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
-            <EmployeesList data={data} />
-            <EmployeesAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
-export default App
+export default App;
