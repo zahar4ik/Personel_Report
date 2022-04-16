0848577068b9433e148f0ddc854e95883147039c
zahar varfolomeev <83136866+Lucifer000014@users.noreply.github.com>
2022-04-17 00:28:22 +0300

    commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,8 +13,8 @@ class App extends Component {
         super(props);
         this.state = {
             data: [
-                {name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
-                {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
+                {name: 'John C.', salary: 800, increase: false, rise: false, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: false, rise: false, id: 2},
                 {name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}
             ]
         }
@@ -46,23 +46,12 @@ class App extends Component {
     }
 
     onToggleIncrease = (id) => {
-        // this.setState(({data}) => {
-        //     const index = data.findIndex(elem => elem.id === id);
-        //
-        //     const old = data[index];
-        //     const newItem = {...old, increase: !old.increase};
-        //     const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
-        //
-        //     return {
-        //         data: newArr
-        //     }
-        // })
         this.setState(({data}) => ({
             data: data.map(item => {
                 if (item.id === id) {
                     return {...item, increase: !item.increase}
                 }
-                return {item}
+                return item;
             })
         }))
     }
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,20 +1,18 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
-import {Fragment} from "react";
 
 const EmployeesList = ({data, onDelete, onToggleIncrease, onToggleRise}) => {
 
     const elements = data.map(item => {
         const {id, ...itemProps} = item;
         return (
-            <Fragment key={id}>
             <EmployeesListItem
+                key={id}
                 {...itemProps}
                 onDelete={() => onDelete(id)}
                 onToggleIncrease={() => onToggleIncrease(id)}
                 onToggleRise={() => onToggleRise(id)}/>
-            </Fragment>
         )
     })
 
